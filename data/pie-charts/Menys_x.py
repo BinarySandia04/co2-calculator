7507308198fda126c73d9bb1171a43a1377a2efc
arnau9182756 <150519380+arnau9182756@users.noreply.github.com>
2023-11-11 20:46:18 +0100

    Create Menys_x.py

---
+++ data/pie-charts/Menys_x.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('Datathon_Results_MOBILITY_2022_original_Students.csv')
+
+transport_types_inverted = {
+    'On foot': 'active',
+    'Bicycle': 'active',
+    'Bus': 'public',
+    'Renfe': 'public',
+    'Underground': 'public',
+    'FGC': 'public',
+    'Tram': 'public',
+    'Combustion vehicle (non-plug-in hybrid, electric or plug-in hybrid with non-renewable source charging),': 'private',
+    'Combustion or electric motorcycle with non-renewable source charging': 'private',
+    'Scooter (or other micro-mobility devices) with renewable charging': 'private',
+    'Taxi': 'private',
+    'Electric vehicle (with Zero label and renewable source charging)': 'private',
+    'Scooter (or other micro-mobility devices) with non-renewable charging': 'private',
+    'Electric motorcycle': 'private',
+}
+
+def get_transport_type(t):
+    try:
+        return transport_types_inverted[t]
+    except:
+        return None
+
+df["transport_types"] = df[df.columns[7]].apply(get_transport_type)
+
+cpuni = pd.read_csv("cp-uni.csv")
+
+dg = pd.merge(
+    df["transport_types"],
+    cpuni["dist"],
+    left_index=True,
+    right_index=True
+)
+dg.head()
+
+print("hola")
+for x in [3, 10, 25]:
+    dg_menys_x = dg[dg["dist"]<=x]
+    print("hola")
+    print(dg_menys_x["transport_types"].count_values())
+    
+    #plt.pie(reasons_type.values(), labels = reasons_type.keys())
+    #plt.savefig(f"menys de {x}")
